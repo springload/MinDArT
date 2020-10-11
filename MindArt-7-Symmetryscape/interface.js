e981c88fd9b49c7b6da647f69fdc29d9d28ba97f
windowtrap <38334931+windowtrap@users.noreply.github.com>
2020-10-11 13:34:34 +1300

    Implement app 7

---
+++ MindArt-7-Symmetryscape/interface.js
@@ -0,0 +1,211 @@
+
+let saveButton, newButton, button3, button, fsButton;
+let fsBool = 0;
+
+let rectWidth;
+let counter = 4; // so that when the restart happens, resets to 0 via the restart function.
+let uiInterrupt = 0;
+
+let colArray = ["#000000", "#444444", "#888888", "#a1a1a1", "#c2c2c2", "#ffffff"]
+
+function writeTextUI() {
+
+  textSize(longEdge / 50);
+  fill(0);
+  noStroke();
+
+  newButton = createButton("Next")
+  newButton.class("select");
+  newButton.position(width - (15 * vMax), height - (12.5 * vMax));
+  newButton.style('font-size', '2.6vmax');
+  newButton.style('height', '4.5vmax');
+  newButton.mousePressed(restart);
+
+  saveButton = createButton("Save")
+  saveButton.class("select");
+  saveButton.style('font-size', '2.6vmax');
+  saveButton.style('height', '4.5vmax');
+  saveButton.position(width - (15 * vMax), height - (6.5 * vMax));
+  saveButton.mousePressed(saveImg);
+
+  button = createImg('assets/eraseOn.png');
+  button.remove();
+  button = createImg('assets/eraseOff.png');
+  button.position(0.8 * vMax, height - (14 * vMax));
+  button.size(14 * vMax, 14 * vMax);
+  button.mousePressed(eraser);
+
+    swatch1 = createButton("");
+    swatch1.position(13 * vMax, height - (13 * vMax));
+    swatch1.size(7 * vMax, 10.5 * vMax);
+    swatch1.style("background-color", colArray[0]);
+    swatch1.class("box");
+    swatch1.mousePressed(function() {
+      changeBrush(1)
+    });
+
+    swatch2 = createButton("");
+    swatch2.position(20 * vMax, height - (13 * vMax));
+    swatch2.size(7 * vMax, 10.5 * vMax);
+    swatch2.style("background-color", colArray[1]);
+    swatch2.class("box");
+    swatch2.mousePressed(function() {
+      changeBrush(2)
+    });
+
+    swatch3 = createButton("");
+    swatch3.position(27 * vMax, height - (13 * vMax));
+    swatch3.size(7 * vMax, 10.5 * vMax);
+    swatch3.style('background-color', colArray[2]);
+    swatch3.class("box");
+    swatch3.mousePressed(function() {
+      changeBrush(3)
+    });
+
+    swatch4 = createButton("");
+    swatch4.position(34 * vMax, height - (13 * vMax));
+    swatch4.size(7 * vMax, 10.5 * vMax);
+    swatch4.style("background-color", colArray[3]);
+    swatch4.class("box");
+    swatch4.mousePressed(function() {
+      changeBrush(4)
+    });
+
+    swatch5 = createButton("");
+    swatch5.position(41 * vMax, height - (13 * vMax));
+    swatch5.size(7 * vMax, 10.5 * vMax);
+    swatch5.style("background-color", colArray[4]);
+    swatch5.class("box");
+    swatch5.mousePressed(function() {
+      changeBrush(5)
+    });
+
+    swatch6 = createButton("");
+    swatch6.position(48 * vMax, height - (13 * vMax));
+    swatch6.size(7 * vMax, 10.5 * vMax);
+    swatch6.style("background-color", colArray[5]);
+    swatch6.class("box");
+    swatch6.mousePressed(function() {
+      changeBrush(6)
+    });
+
+    selColour = createImg('assets/colSelected.png');
+    selColour.position(13 * vMax, height - (16 * vMax));
+    selColour.size(7 * vMax, 16 * vMax);
+    selColour.mousePressed();
+
+
+}
+
+
+
+function eraser(){
+
+brushSelected = 6;
+button.remove();
+button = createImg('assets/eraseOn.png');
+button.position(0.8 * vMax, height - (14 * vMax));
+button.size(14 * vMax, 14 * vMax);
+button.mousePressed(eraser);
+selColour.remove();
+
+  }
+
+
+  function changeBrush(brushSel) {
+    click.play();
+    button.remove();
+    button = createImg('assets/eraseOff.png');
+    button.position(0.8 * vMax, height - (14 * vMax));
+    button.size(14 * vMax, 14 * vMax);
+    button.mousePressed(eraser);
+
+    brushSelected = brushSel-1;
+
+    selColour.remove();
+    selColour = createImg('assets/colSelected.png');
+    selColour.position((13 + ((brushSel-1) * 7)) * vMax, height - (16 * vMax));
+    selColour.size(7 * vMax, 16 * vMax);
+    selColour.mousePressed();
+
+  }
+
+
+
+function interruptor() {
+  uiInterrupt= 1;
+}
+
+
+function restartTimeout(){
+  click.play();
+  setTimeout(restart, 250);
+}
+
+function restart() {
+  counter++;
+
+  fill(0);
+  dimensionCalc();
+  intX = width / 5
+  intY = height / 2;
+
+  drawState = 1;
+  drawLayer.clear();
+  uiLayer.clear();
+
+  if (counter >= 4){
+    counter = 0;
+  }
+
+  if (counter === 1 || counter === 2){
+  uiLayer.strokeWeight(1);
+  uiLayer.stroke(210);
+  uiLayer.line(0, height/2, width, height/2);
+}
+
+
+  if (counter === 0 || counter === 2){
+  uiLayer.strokeWeight(1);
+  uiLayer.stroke(210);
+  uiLayer.line(width/2, 0, width/2, height);
+}
+
+if (counter === 3){
+uiLayer.strokeWeight(1);
+uiLayer.stroke(210);
+uiLayer.line(0, 0, width, height);
+uiLayer.line(width, 0, 0, height);
+}
+
+
+render();
+
+}
+
+function saveImg() {
+    click.play();
+  image(bg, 0, 0, width, height);
+  image(drawLayer, 0, 0, width, height);
+  save('SymmetryScape' + month() + day() + hour() + second() + '.jpg');
+}
+
+function checkFS(){
+  if (!fullscreen()){
+  addFS();
+}
+}
+
+function addFS(){
+  $('.fsButton').remove();
+  fsButton = createImg('assets/enterFS.png', "FULLSCREEN");
+  fsButton.style('height', '4.5vMax');
+  fsButton.class("fsButton");
+  fsButton.position(width - (7.5 * vMax), 1.5 * vMax);
+  fsButton.mousePressed(fs);
+}
+
+function fs(){
+  fullscreen(1);
+  $('.fsButton').remove();
+}
---
+++ MindArt-7-Symmetryscape/symmetryscape.js
@@ -0,0 +1,258 @@
+let brush = [];
+let longEdge, shortEdge, circleRad, vMax, wmax, hmax;
+let drawLayer, uiLayer;
+let brushSelected = 0;
+let faderStart;
+let xCo = [];
+let yCo = [];
+let velo = [];
+let brushBool = 0;
+let intX, intY;
+let eraseAlpha;
+let storedOrientation, storedOrientationDegrees, rotateDirection;
+
+function preload() {
+  bg = loadImage('assets/paper.jpg');
+  audio = loadSound('assets/audio.mp3');
+  click = loadSound('assets/click.mp3');
+  eraseAlpha = loadImage('assets/eraseAlpha3.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(1); // Ignores retina displays
+  drawLayer = createGraphics(width, height);
+  drawLayer.colorMode(RGB, 255, 255, 255, 1000);
+  drawLayer.strokeCap(PROJECT);
+  uiLayer = createGraphics(width, height);
+
+
+
+    var stbtn = $("<div />").appendTo("body");
+    stbtn.addClass('startBtn');
+    $('<p>Touch here to begin</p>').appendTo(stbtn);
+    stbtn.mousedown(start);
+    stbtn.mousemove(start);
+
+}
+
+function start() {
+
+  $('.startBtn').remove();
+  fullscreen(1);
+
+  if (audio.isPlaying()) {} else {
+    audio.loop(1);
+  }
+
+  restart();
+
+
+
+
+
+}
+
+
+
+function dimensionCalc() {
+  wmax = width / 100;
+  hmax = height / 100;
+  if (width > height) {
+    longEdge = width;
+    shortEdge = height;
+    circleRad = shortEdge * 0.45;
+    vMax = width / 100;
+  } else {
+    longEdge = height;
+    shortEdge = width;
+    vMax = height / 100;
+    circleRad = shortEdge * 0.45;
+  }
+}
+
+function mousePressed() {
+  faderStart = 600;
+
+  return false;
+}
+
+
+
+
+
+function touchMoved() {
+  makeDrawing(winMouseX, winMouseY, pwinMouseX, pwinMouseY);
+    render();
+  return false;
+}
+
+function makeDrawing(_x, _y, pX, pY) {
+  drawLayer.strokeWeight(constrain(abs((_y + _x) - (pX + pY)), 1, 2)); // for line work
+  drawLayer.stroke(0);
+  if (counter === 0) {
+    brushIt(_x, _y, pX, pY);
+    brushIt(width - _x, _y, width - pX, pY);
+  } else if (counter === 1) {
+    brushIt(_x, _y, pX, pY);
+    brushIt(_x, height - _y, pX, height - pY);
+  } else if (counter === 2) {
+    brushIt(_x, _y, pX, pY);
+    brushIt(width - _x, _y, width - pX, pY);
+    brushIt(_x, height - _y, pX, height - pY);
+    brushIt(width - _x, height - _y, width - pX, height - pY);
+  } else if (counter === 3) {
+    drawLayer.push();
+    brushIt(_x, _y, pX, pY);
+    drawLayer.translate(width / 2, height / 2);
+    drawLayer.rotate(PI * 0.5);
+    drawLayer.translate(-width / 2, -height / 2);
+    brushIt(_x, _y, pX, pY);
+    drawLayer.translate(width / 2, height / 2);
+    drawLayer.rotate(PI * 01);
+    drawLayer.translate(-width / 2, -height / 2);
+    brushIt(_x, _y, pX, pY);
+    drawLayer.translate(width / 2, height / 2);
+    drawLayer.rotate(PI * 1.5);
+    drawLayer.translate(-width / 2, -height / 2);
+    brushIt(_x, _y, pX, pY);
+    drawLayer.pop();
+  }
+}
+
+function brushIt(_x, _y, pX, pY) {
+  if (brushSelected === 3) {
+    drawLayer.strokeWeight(constrain(abs((_y + _x) - (pX + pY)), 2, 3)); // for line work
+    drawLayer.stroke(100, 100, 100, 50);
+    for (i = 0; i < 10; i++) {
+      let randX = randomGaussian(-6, 6);
+      let randY = randomGaussian(-6, 6);
+      drawLayer.line(_x + randX, _y + randY, pX + randX, pY + randY);
+    }
+  }
+  if (brushSelected === 1) {
+    drawLayer.strokeWeight(constrain(abs((_y + _x) - (pX + pY)), 3, 5)); // for line work
+    drawLayer.stroke(10, 10, 10, 60);
+    drawLayer.line(_x, _y, pX, pY);
+  }
+  if (brushSelected === 0) {
+    drawLayer.strokeWeight(constrain(abs((_y + _x) - (pX + pY)), 14, 15)); // for line work
+    drawLayer.stroke(20, 20, 20, 50);
+    drawLayer.line(_x, _y, pX, pY);
+  } else if (brushSelected === 4) {
+    drawLayer.strokeWeight(abs(random(0, 4)));
+    for (i = 0; i < 60; i++) {
+      let tempCol = abs(random(200, 255));
+      drawLayer.stroke(tempCol, tempCol, tempCol, 100);
+      drawLayer.point(_x + randomGaussian(-10, 10), _y + randomGaussian(-10, 10));
+    }
+  } else if (brushSelected === 5) {
+    drawLayer.strokeWeight(constrain(abs((_y + _x) - (pX + pY)), 30, 40)); // for line work
+    drawLayer.stroke(255, 255, 255, 35);
+    drawLayer.line(_x, _y, pX, pY);
+  } else if (brushSelected === 2) {
+    drawLayer.strokeWeight(constrain(abs((_y + _x) - (pX + pY)), 50, 60)); // for line work
+    if (faderStart <= 0) {
+      brushBool = 0;
+    }
+    if (faderStart >= 1000) {
+      brushBool = 1;
+    }
+    if (brushBool === 0) {
+      drawLayer.stroke(100, 100, 100, (faderStart += 20) / 50);
+    }
+    if (brushBool === 1) {
+      drawLayer.stroke(100, 100, 100, (faderStart -= 20) / 50);
+    }
+    drawLayer.line(_x, _y, pX, pY);
+  } else if (brushSelected === 6) {
+    drawLayer.blendMode(REMOVE);
+
+    drawLayer.image(eraseAlpha, _x - 50, _y - 50, 100, 100);
+    drawLayer.blendMode(BLEND);
+
+  }
+}
+
+
+
+function render() {
+  image(bg, 0, 0, width, height);
+  image(drawLayer, 0, 0, width, height);
+  image(uiLayer, 0, 0, width, height);
+}
+
+function windowResized() {
+
+  resizeCanvas(windowWidth, windowHeight);
+  sizeWindow();
+  drawLayer.strokeCap(PROJECT);
+  dimensionCalc();
+  removeElements(); // todo, incorp into writeTextUI
+  writeTextUI();
+  checkFS();
+  render();
+}
+
+
+function sizeWindow() {
+  if (width < height) {
+    currentOrientation = "portrait";
+  } else {
+    currentOrientation = "landscape";
+  }
+  if (currentOrientation === storedOrientation) {
+    stretchWindow();
+  } else {
+    if (window.orientation < storedOrientationDegrees) {
+      direction = 1;
+    } else {
+      direction = -1;
+    }
+
+    if (abs(window.orientation - storedOrientationDegrees) == 270){
+      direction = -direction;
+    }
+    rotateWindow(direction);
+    storedOrientationDegrees = window.orientation;
+  }
+  storedOrientation = currentOrientation;
+}
+
+
+function stretchWindow() {
+  uiLayer = createGraphics(width, height);
+  var newdrawLayer = createGraphics(windowWidth, windowHeight);
+  newdrawLayer.image(drawLayer, 0, 0, windowWidth, windowHeight);
+  drawLayer.resizeCanvas(windowWidth, windowHeight);
+  drawLayer = newdrawLayer;
+  newdrawLayer.remove();
+}
+
+function rotateWindow(direction) {
+  uiLayer = createGraphics(width, height);
+  var newdrawLayer = createGraphics(windowWidth, windowHeight);
+  newdrawLayer.push();
+  newdrawLayer.translate(width / 2, height / 2);
+  newdrawLayer.rotate((PI / 2) * direction);
+  newdrawLayer.translate(-height / 2, -width / 2);
+  newdrawLayer.image(drawLayer, 0, 0, windowHeight, windowWidth);
+  newdrawLayer.pop()
+  drawLayer.resizeCanvas(windowWidth, windowHeight);
+  drawLayer = newdrawLayer;
+  newdrawLayer.remove();
+
+  // TODO: properly detect the orientation
+  rotateDirection = rotateDirection * -1;
+}
+
+//startSimulation and pauseSimulation defined elsewhere
+function handleVisibilityChange() {
+  if (document.hidden) {
+    audio.stop();
+  } else {
+    audio.loop(1);
+  }
+}
+
+document.addEventListener("visibilitychange", handleVisibilityChange, false);
