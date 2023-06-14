fdf98e7ecce5ff539578c75b50c80301a5150e47
jameswilce <jameswilce@gmail.com>
2023-06-14 19:29:51 +1200

    completed updates to service worker

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: '.',
 	globPatterns: [
-		'**/*.{jpg,otf,png,mp3,js,html,css,ico,eot,svg,ttf,woff,woff2,manifest,properties,psd,json,md,ts}'
+		'**/*.{jpg,otf,png,mp3,js,html,css,json,ico,eot,svg,ttf,woff,woff2,manifest,properties,psd,md,ts}'
 	],
 	swDest: 'sw.js',
 	swSrc: 'sw.js',
