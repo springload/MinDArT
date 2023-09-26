47128349ed11c421e47a4ff399ce915580421673
jameswilce <jameswilce@gmail.com>
2023-09-26 20:40:30 +1300

    Rebuild before deploy

--- functions.js
+++ functions.js
@@ -1,20 +1,21 @@
 function Programme_AppRelease(){
 
     // set programme start date (mm/dd/yyyy)
-    var date = new Date("07/01/2023");
+    var date = new Date("26/09/2023");
 
     // Calculate the number of elapsed days since programme start
     var today = new Date();
     var elapsedDays = Math.floor((today - date) / 86400000);
 
-    var wk1 = '<div class="grid-Active"><a href="MinDArT-1-Touchscape/index.html"><img class="session" src="assets/1.jpg" alt="Week 1 - Touchscape"></a></div>';
-    var wk2 = '<div class="grid-Active"><a href="MinDArT-2-Linescape/index.html"><img class="session" src="assets/2.jpg" alt="Week 2 - Linescape"></a></div>';
-    var wk3 = '<div class="grid-Active"><a href="MinDArT-3-Circlescape/index.html"><img class="session" src="assets/3.jpg" alt="Week 3 - Circlescape"></a></div>';
-    var wk4 = '<div class="grid-Active"><a href="MinDArT-4-Colourscape/index.html"><img class="session" src="assets/4.jpg" alt="Week 4 - Colourscape"></a></div>';
-    var wk5 = '<div class="grid-Active"><a href="MinDArT-5-Dotscape/index.html"><img class="session" src="assets/5.jpg" alt="Week 5 - Dotscape"></a></div>';
-    var wk6 = '<div class="grid-Active"><a href="MinDArT-6-Linkscape/index.html"><img class="session" src="assets/6.jpg" alt="Week 6 - Linkscape"></a></div>';
-    var wk7 = '<div class="grid-Active"><a href="MinDArT-7-Rotationscape/index.html"><img class="session" src="assets/7.jpg" alt="Week 7 - Rotationscape"></a></div>';
-    var wk8 = '<div class="grid-Active"><a href="MinDArT-8-Symmetryscape/index.html"><img class="session" src="assets/8.jpg" alt="Week 8 - Symmetryscape"></a></div>';
+
+    var wk1 = '<div class="grid-Active"><a href="MinDArT-1-Touchscape/index.html"><img class="session" src="assets/touch scape icon.png" alt="Week 1 - Touchscape"></a></div>';
+    var wk2 = '<div class="grid-Active"><a href="MinDArT-2-Linescape/index.html"><img class="session" src="assets/line scape icon.png" alt="Week 2 - Linescape"><p></p></a></div>';
+    var wk3 = '<div class="grid-Active"><a href="MinDArT-3-Circlescape/index.html"><img class="session" src="assets/circle scape icon.png" alt="Week 3 - Circlescape"><p></a></p></div>';
+    var wk4 = '<div class="grid-Active"><a href="MinDArT-4-Colourscape/index.html"><img class="session" src="assets/colour scape icon.png" alt="Week 4 - Colourscape"></a></div>';
+    var wk5 = '<div class="grid-Active"><a href="MinDArT-5-Dotscape/index.html"><img class="session" src="assets/dot scape icon.png" alt="Week 5 - Dotscape"></a></div>';
+    var wk6 = '<div class="grid-Active"><a href="MinDArT-6-Linkscape/index.html"><img class="session" src="assets/link scape icon.png" alt="Week 6 - Linkscape"><p></a></p></div>';
+    var wk7 = '<div class="grid-Active"><a href="MinDArT-7-Rotationscape/index.html"><img class="session" src="assets/rotation scape icon.png" alt="Week 7 - Rotationscape"><p></a></p></div>';
+    var wk8 = '<div class="grid-Active"><a href="MinDArT-8-Symmetryscape/index.html"><img class="session" src="assets/symmetry scape icon.png" alt="Week 8 - Symmetryscape"></a></div>';
 
         // determine apps to display
         if (elapsedDays < 7) {
