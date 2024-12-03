a5f0b710765099ddb1228ad86bed4ab3d66ef342
Sam Muirhead <sam.muirhead@springload.co.nz>
2024-12-03 19:48:52 +1300

    Extract creation of app controls

--- MinDArT-1-Touchscape/interface-touch.js
+++ MinDArT-1-Touchscape/interface-touch.js
@@ -11,86 +11,82 @@ function calcDimensions() {
 }
 
 function writeTextUI() {
+  document.querySelectorAll(".interface").forEach((el) => el.remove());
 
-  // TODO: REMOVE ELEMENTS
+  const interfaceElement = document.getElementById("interface" + i);
+  if (interfaceElement) {
+    interfaceElement.addEventListener(
+      "touchstart",
+      (event) => {
+        rake(i);
+        event.preventDefault();
+      },
+      { passive: false }
+    );
 
-$(".interface").remove();
-
-    $('#interface'+i).on("touchstart", function(event){
-      rake(i);
-      event.preventDefault();
-    }, {passive: false});
-
-    $('#interface'+i).on("click", function(event){
-      rake(i);
-      event.preventDefault();
-    }, {passive: false});
-
-    show_btns();
-
-// Add active class to the current button (highlight it)
-var header = document.getElementById("myDIV");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  if (current.length > 0) { 
-    current[0].className = current[0].className.replace(" active", "");
+    interfaceElement.addEventListener(
+      "click",
+      (event) => {
+        rake(i);
+        event.preventDefault();
+      },
+      { passive: false }
+    );
   }
-  this.className += " active";
-  });
-}
 
-// Buttons on the right..
-homeButton = createButton('Main Menu');
-homeButton.position(width - (13 * vMax),height - (20 * vMax));
-homeButton.class('right-buttons');
-homeButton.mousePressed(menu);  
+  show_btns();
 
-resetButton = createButton('New');
-resetButton.position(width - (13 * vMax),height - (14 * vMax));
-resetButton.class('right-buttons');
-resetButton.mousePressed(resetTimeout);
+  const header = document.getElementById("myDIV");
+  if (header) {
+    const btns = header.getElementsByClassName("btn");
+    Array.from(btns).forEach((btn) => {
+      btn.addEventListener("click", function () {
+        // Remove active class from current active button
+        const current = document.querySelector(".active");
+        if (current) {
+          current.classList.remove("active");
+        }
+        // Add active class to clicked button
+        this.classList.add("active");
+      });
+    });
+  }
 
-saveButton = createButton('Save');
-saveButton.position(width - (13 * vMax),height - (8 * vMax));
-saveButton.class('right-buttons');
-saveButton.mousePressed(saveImg);
-
-
+  createAppControls("touchscape", resetTimeout);
 }
 
 function rake(version) {
-
   currentRake = version;
 
-  if (version == 0) {
-    change(1, 200, 1);
-    eraseActive = 1;
-  }
-  if (version == 1) {
-    change(5, 60, 100);
-    eraseActive = 0;
-  }
-  if (version == 2) {
-    change(15, 100, 100);
-    eraseActive = 0;
-  }
-  if (version == 3) {
-    change(60, 150, 100);
-    eraseActive = 0;
+  switch (version) {
+    case 0:
+      change(1, 200, 1);
+      eraseActive = 1;
+      break;
+    case 1:
+      change(5, 60, 100);
+      eraseActive = 0;
+      break;
+    case 2:
+      change(15, 100, 100);
+      eraseActive = 0;
+      break;
+    case 3:
+      change(60, 150, 100);
+      eraseActive = 0;
+      break;
   }
   click.play();
 }
 
 function saveImg() {
   click.play();
-  save('touchscape' + month() + day() + hour() + second() + '.jpg');
+  save(`touchscape${month()}${day()}${hour()}${second()}.jpg`);
 }
 
 function show_btns() {
-    var x = document.getElementsByClassName("btn");
-    for (var i = 0; i < x.length; i++) {
-      x[i].style.display = "inline";
-    }
-  }
+  const buttons = document.getElementsByClassName("btn");
+  Array.from(buttons).forEach((btn) => {
+    btn.style.display = "grid";
+  });
+}
