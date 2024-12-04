7d933d968dbeceb0f7825fdfa6cefd0c6155225d
Sam Muirhead <sam.muirhead@springload.co.nz>
2024-12-05 11:11:50 +1300

    Include the links in HTML by default, simplify app link code

--- functions.js
+++ functions.js
@@ -1,49 +1,27 @@
-function Programme_AppRelease() {
+function showOnlyCurrentLinks() {
   // set programme start date (mm/dd/yyyy)
-  var date = new Date("02/19/2024");
+  const startDate = new Date("02/19/2024");
+  const dayInMilliseconds = 86400000;
 
   // Calculate the number of elapsed days since programme start
-  var today = new Date();
-  var elapsedDays = Math.floor((today - date) / 86400000);
+  const today = new Date();
+  const elapsedDays = Math.floor((today - startDate) / dayInMilliseconds);
+  const currentWeekNumber = Math.ceil(elapsedDays / 7);
 
-  var wk1 =
-    '<div class="grid-Active"><a href="MinDArT-1-Touchscape/index.html"><img class="session" src="assets/touchscape-icon.png" alt="Week 1 - Touchscape"></a></div>';
-  var wk2 =
-    '<div class="grid-Active"><a href="MinDArT-2-Linescape/index.html"><img class="session" src="assets/linescape-icon.png" alt="Week 2 - Linescape"><p></p></a></div>';
-  var wk3 =
-    '<div class="grid-Active"><a href="MinDArT-3-Circlescape/index.html"><img class="session" src="assets/circlescape-icon.png" alt="Week 3 - Circlescape"><p></a></p></div>';
-  var wk4 =
-    '<div class="grid-Active"><a href="MinDArT-4-Colourscape/index.html"><img class="session" src="assets/colourscape-icon.png" alt="Week 4 - Colourscape"></a></div>';
-  var wk5 =
-    '<div class="grid-Active"><a href="MinDArT-5-Dotscape/index.html"><img class="session" src="assets/dotscape-icon.png" alt="Week 5 - Dotscape"></a></div>';
-  var wk6 =
-    '<div class="grid-Active"><a href="MinDArT-6-Linkscape/index.html"><img class="session" src="assets/linkscape-icon.png" alt="Week 6 - Linkscape"><p></a></p></div>';
-  var wk7 =
-    '<div class="grid-Active"><a href="MinDArT-7-Rotationscape/index.html"><img class="session" src="assets/rotationscape-icon.png" alt="Week 7 - Rotationscape"><p></a></p></div>';
-  var wk8 =
-    '<div class="grid-Active"><a href="MinDArT-8-Symmetryscape/index.html"><img class="session" src="assets/symmetryscape-icon.png" alt="Week 8 - Symmetryscape"></a></div>';
-
-  // determine apps to display
-  if (elapsedDays < 7) {
-    document.querySelector(".grid").innerHTML = wk1;
-  } else if (elapsedDays < 14) {
-    document.querySelector(".grid").innerHTML = wk1 + wk2;
-  } else if (elapsedDays < 21) {
-    document.querySelector(".grid").innerHTML = wk1 + wk2 + wk3;
-  } else if (elapsedDays < 28) {
-    document.querySelector(".grid").innerHTML = wk1 + wk2 + wk3 + wk4;
-  } else if (elapsedDays < 35) {
-    document.querySelector(".grid").innerHTML = wk1 + wk2 + wk3 + wk4 + wk5;
-  } else if (elapsedDays < 42) {
-    document.querySelector(".grid").innerHTML =
-      wk1 + wk2 + wk3 + wk4 + wk5 + wk6;
-  } else if (elapsedDays < 49) {
-    document.querySelector(".grid").innerHTML =
-      wk1 + wk2 + wk3 + wk4 + wk5 + wk6 + wk7;
-  } else {
-    document.querySelector(".grid").innerHTML =
-      wk1 + wk2 + wk3 + wk4 + wk5 + wk6 + wk7 + wk8;
+  const homeGrid = document.querySelector('[data-element="home-grid"]');
+  if (!homeGrid) {
+    console.warn(
+      'Parent element with attribute data-element="home-grid" not found'
+    );
   }
+  const links = homeGrid.querySelectorAll("[data-week]");
+
+  // make any still-to-be-unlocked apps unclickable (opacity will also be reduced in the CSS)
+  links.forEach((link) => {
+    link.dataset.week <= currentWeekNumber
+      ? link.removeAttribute("inert")
+      : link.setAttribute("inert", true);
+  });
 }
 
 // Common interface functions
