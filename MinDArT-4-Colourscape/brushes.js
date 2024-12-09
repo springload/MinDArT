153ba5c6a98a977e40d104c6daed003352f58470
Sam Muirhead <sam.muirhead@springload.co.nz>
2024-12-09 13:58:42 +1300

    Reorganise colourscape files

--- MinDArT-4-Colourscape/brushes.js
+++ MinDArT-4-Colourscape/brushes.js
@@ -38,3 +38,18 @@ function drawErase() {
   eraserVersion = false;
   click.play();
 }
+
+function eraseDrawing() {
+  if (eraserVersion) {
+    paintLayer.noStroke();
+    paintLayer.strokeWeight(45);
+    paintLayer.stroke(255, 255, 255, 125);
+    paintLayer.line(mouseX, mouseY, pmouseX, pmouseY);
+  } else {
+    traceLayer.blendMode(BLEND);
+    traceLayer.strokeWeight(45);
+    traceLayer.stroke(255, 0, 0, 0.4);
+    traceLayer.line(mouseX, mouseY, pmouseX, pmouseY);
+    traceLayer.blendMode(LIGHTEST);
+  }
+}
