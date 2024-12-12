0eaf45503045285b302b742761aa15bd3d1df484
Sam Muirhead <sam.muirhead@springload.co.nz>
2024-12-12 16:36:14 +1300

    Use same strategy for code as assets

--- sw-source.js
+++ sw-source.js
@@ -1,5 +1,5 @@
 import { Serwist } from "serwist";
-import { CacheFirst, NetworkFirst } from "@serwist/strategies";
+import { CacheFirst } from "@serwist/strategies";
 import { ExpirationPlugin } from "@serwist/expiration";
 import { CacheableResponsePlugin } from "@serwist/cacheable-response";
 import { Route } from "@serwist/routing";
@@ -19,35 +19,13 @@ const sw = new Serwist({
   },
 });
 
-// For runtime caching
+// Use CacheFirst for all files
 sw.registerRoute(
   new Route(
-    ({ url }) => url.pathname.match(/\.(js|html|css)$/),
-    new NetworkFirst({
-      cacheName: "mindart-content",
-      plugins: [
-        new CacheableResponsePlugin({
-          statuses: [0, 200],
-        }),
-        {
-          cacheWillUpdate: async ({ request, response }) => {
-            // Return the response directly without modification
-            return response;
-          },
-        },
-      ],
-    })
-  )
-);
-
-// Register other asset route
-sw.registerRoute(
-  new Route(
-    ({ url }) => {
-      return url.pathname.match(/\.(png|jpg|jpeg|svg|gif|mp3|woff2)$/);
-    },
+    ({ url }) =>
+      url.pathname.match(/\.(js|html|css|png|jpg|jpeg|svg|gif|mp3|woff2)$/),
     new CacheFirst({
-      cacheName: "mindart-assets",
+      cacheName: "mindart-all",
       plugins: [
         new ExpirationPlugin({
           maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
