3d2673aa02716d829672b533ed614fecd18363f0
Sam Muirhead <sam.muirhead@springload.co.nz>
2025-02-05 11:41:27 +1300

    Switch to vite-pwa

--- js/apps/6-linkscape.js
+++ js/apps/6-linkscape.js
@@ -59,8 +59,8 @@ export function createLinkscape(p5) {
   };
 
   function preload() {
-    state.texture = p5.loadImage("assets/6-linkscape_texture.webp");
-    state.pin = p5.loadImage("assets/6-linkscape_pin.webp");
+    state.texture = p5.loadImage("images/6-linkscape_texture.webp");
+    state.pin = p5.loadImage("images/6-linkscape_pin.webp");
   }
 
   async function setup() {
