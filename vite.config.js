7b1a10c11da46b7e81ff3fc2072fac674a6ec19b
Sam Muirhead <sam.muirhead@springload.co.nz>
2025-02-13 12:14:10 +1300

    Restart with new vite config

--- vite.config.js
+++ vite.config.js
@@ -1,58 +1,9 @@
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/MinDArT/",
   build: {
-    chunkSizeWarningLimit: 1200,
-    sourcemap: true,
-    copyPublicDir: true,
+    outDir: "dist",
   },
-  publicDir: "public",
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: [
-        "assets/*.webp",
-        "assets/*.png",
-        "sound/*.mp3",
-        "mindArt_Logo.png",
-      ],
-      manifest: {
-        name: "MinDArT",
-        short_name: "MinDArT",
-        description: "Mind art progressive web app, for offline use",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "fullscreen",
-        icons: [
-          {
-            src: "assets/mindArt_Logo_96.png",
-            sizes: "96x96",
-            type: "image/png",
-          },
-          {
-            src: "assets/mindArt_Logo_152.png",
-            sizes: "152x152",
-            type: "image/png",
-          },
-          {
-            src: "assets/mindArt_Logo_384.png",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "assets/mindArt_Logo_384.png",
-            sizes: "384x384",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-        start_url: "/",
-        scope: "/",
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,webp,png,mp3,woff2}"],
-      },
-    }),
-  ],
+  publicDir: "assets",
 });
