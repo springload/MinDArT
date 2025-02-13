382665d0f5afd9d6116ccc6c1f9791a2e9f11145
Sam Muirhead <sam.muirhead@springload.co.nz>
2025-02-13 15:21:36 +1300

    Update main menu link to use SPA routing

--- js/components/app-controls.js
+++ js/components/app-controls.js
@@ -24,6 +24,7 @@ class AppControls extends HTMLElement {
   setupEventListeners() {
     const resetButton = this.querySelector('[data-element="reset-button"]');
     const saveButton = this.querySelector('[data-element="save-button"]');
+    const homeLink = this.querySelector('a[href="/"]');
 
     const actions = {
       reset: () => {
@@ -34,6 +35,13 @@ class AppControls extends HTMLElement {
         playClick();
         this.dispatchEvent(new CustomEvent("app-save"));
       },
+      home: (e) => {
+        e.preventDefault();
+        playClick();
+        window.history.pushState({}, "", "/");
+        // This will trigger the router's updateView()
+        window.dispatchEvent(new PopStateEvent("popstate"));
+      },
     };
 
     if (resetButton) {
@@ -42,6 +50,10 @@ class AppControls extends HTMLElement {
 
     if (saveButton) {
       addInteractionHandlers(saveButton, actions.save);
+    }
+
+    if (homeLink) {
+      addInteractionHandlers(homeLink, actions.home);
     }
   }
 }
